093e01dfe0154e8c5d219118534aee8436100aac
Lucas Goldman <lucasgoldman@optonline.net>
2024-03-14 17:19:29 -0500

    ByteBracket Utils: Upload season data to s3

--- utilities/extract_team_data.py
+++ utilities/extract_team_data.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import boto3
+from io import StringIO
+
+BUCKET_NAME = 'bytebracket-ncaab-data'
+FILE_KEY = 'ncaab-data.csv'
 
 def extract_team_data():
     url = "https://www.sports-reference.com/cbb/seasons/men/2024-school-stats.html#basic_school_stats"
@@ -21,4 +26,21 @@ def extract_team_data():
 
     teams_df = pd.DataFrame(teams)
     teams_df.set_index('school_name', inplace=True)
-    teams_df.to_csv('teams.csv')
+    teams_df.drop(['g', 'srs', 'wins_conf', 'losses_conf', 'wins_home', 'losses_home', 'wins_visitor', 'losses_visitor', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fta', 'ft', 'orb', 'pf'], axis=1, inplace=True)
+    return teams_df
+
+def upload_to_s3():
+    teams_df = extract_team_data()
+
+    # Convert DataFrame to CSV string
+    csv_buffer = StringIO()
+    teams_df.to_csv(csv_buffer, index=False)
+
+    # Initialize S3 client
+    s3 = boto3.client('s3')
+
+    # Upload CSV string to S3
+    s3.put_object(Bucket=BUCKET_NAME, Key=FILE_KEY, Body=csv_buffer.getvalue())
+
+if __name__ == '__main__':
+    upload_to_s3()
